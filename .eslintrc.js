a92802a74a968fdc45fa4dd442448abc45b2ca12
Thiago Durante <thdurante@gmail.com>
2020-05-20 14:19:37 -0300

    Setup prettier.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,47 +1,46 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es6': true
+  env: {
+    browser: true,
+    es6: true,
   },
-  'extends': [
+  extends: [
     'plugin:react/recommended',
     'airbnb',
-    'plugin:@typescript-eslint/recommended'
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
   ],
-  'globals': {
-    'Atomics': 'readonly',
-    'SharedArrayBuffer': 'readonly'
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
   },
-  'parser': '@typescript-eslint/parser',
-  'parserOptions': {
-    'ecmaFeatures': {
-      'jsx': true
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    'ecmaVersion': 11,
-    'sourceType': 'module'
+    ecmaVersion: 11,
+    sourceType: 'module',
   },
-  'plugins': [
-    'react',
-    'react-hooks',
-    '@typescript-eslint'
-  ],
-  'rules': {
+  plugins: ['react', 'react-hooks', '@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
-    'react/jsx-filename-extension': [1, { 'extensions': ['.tsx'] }],
+    'react/jsx-filename-extension': [1, { extensions: ['.tsx'] }],
     'import/prefer-default-export': 'off',
     'import/extensions': [
       'error',
       'ignorePackages',
       {
-        'ts': 'never',
-        'tsx': 'never'
-      }
-    ]
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
   },
-  'settings': {
+  settings: {
     'import/resolver': {
-      'typescript': {}
-    }
-  }
+      typescript: {},
+    },
+  },
 };
